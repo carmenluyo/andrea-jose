f2a00c403a9283844119ebd3ddcd3ef4302191cb
Carmen Luyo <c.luyorosales@gmail.com>
2020-07-27 15:50:00 -0500

    se agrego slider

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 
 $(document).ready(function() {
    $('.owl-carousel').owlCarousel({
-        margin:10,
-        loop:true,
-        autoWidth:true,
-        items:4
+	    margin:10,
+	    nav:true,
+        items:1,
     })
 });
