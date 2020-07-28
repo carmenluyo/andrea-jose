382395da3b7544fa2dcb1c29d8c01cf106104947
Carmen Luyo <c.luyorosales@gmail.com>
2020-07-28 16:45:10 -0500

    se agrego menu

--- js/main.js
+++ js/main.js
@@ -1,8 +1,38 @@
 
 $(document).ready(function() {
-   $('.owl-carousel').owlCarousel({
+    $('.owl-carousel').owlCarousel({
 	    margin:10,
 	    nav:true,
         items:1,
     })
+
+    $(document).on('click', '.menu-toggler', function() {
+        
+    })
+
+    const   body = 'body',
+            openMenu = '.menu-toggler',
+            closeMenu = '.close-menu',
+            menuOverlay = '.menu-overlay',
+            menuNav = '.menu-nav'
+
+
+    toggleMenu(openMenu)
+    toggleMenu(closeMenu)
+    toggleMenu(menuOverlay)
+
+    function toggleMenu(toggler) {
+        $(document).find(toggler).each(function() {
+            $(this).on("click", function(e){
+                e.preventDefault();
+                $(menuOverlay).toggleClass('active')
+                $(menuNav).toggleClass('active')
+                $(closeMenu).toggleClass('active')
+                setTimeout(
+                    function () {
+                        $(body).toggleClass('overflow-y-hidden')
+                    }, 300)
+            });
+        });
+    }
 });
